0165e0be3c053e4faaabac393034a04dc00471fc
ligang25 <ligang25@58.com>
2020-10-15 11:22:19 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,13 @@
 module.exports = {
   title: 'Cola',
   description: 'Cola Cat',
-  base:'/cola/'
+  base:'/cola/',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: 'HTML', link: '/html/' },
+      { text: 'CSS', link: '/CSS/' },
+      { text: 'JavaScript', link: '/JavaScript/' }
+    ]
+  }
 }
